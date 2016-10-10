378ea9dfb1371801ebfc87727f8410e7ffc0eb5a
jpatriciodasilva <jpatriciodasilva@nautilus10.danielcastelao.org>
2016-10-10 10:25:03 +0200

    FINAL

---
+++ src/ejercicio1psp/Ejercicio1PSP.java
@@ -0,0 +1,16 @@
+
+package ejercicio1psp;
+
+
+public class Ejercicio1PSP {
+
+    
+    public static void main(String[] args) {
+      new MiThread("  thread1").start();
+      
+      new MiThread("  thread2").start();
+      
+    }
+    
+    
+}
